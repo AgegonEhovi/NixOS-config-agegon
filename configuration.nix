868511d4d7d96a76a1c82ae7cc5717fa68adf6f1
agegon <feedcreep@yandex.ru>
2025-06-29 15:24:37 +0300

    add lutris

--- configuration.nix
+++ configuration.nix
@@ -70,29 +70,36 @@
   # Enable networking
   networking.networkmanager.enable = true;
 
+  # Enable networking
+  networking.networkmanager.enable = true;
+  
   networking = {
     firewall = {
       enable = true;
-    
+      
       # Разрешить базовые службы
       allowedTCPPorts = [ 22 ];   # SSH
-    
-      allowedUDPPorts = [ 5353 ];  # mDNS (обнаружение устройств в сети)
-
+      
+      allowedUDPPorts = [ 
+        5353   # mDNS (обнаружение устройств в сети)
+        9993   # ZeroTier
+      ];
+  
       # Разрешить виртуализацию
       extraCommands = ''
         # Разрешить трафик для libvirt/KVM
         iptables -I FORWARD -i virbr0 -j ACCEPT
         iptables -I FORWARD -o virbr0 -j ACCEPT
-      
+        
         # Разрешить доступ к SPICE (виртуальные машины)
         iptables -A INPUT -p tcp --dport 5900:6900 -j ACCEPT
       '';
-    
+      
       # Разрешить ping
       allowPing = true;
     };
   };
+
 
   # Set your time zone.
   time.timeZone = "Europe/Moscow";
@@ -194,6 +201,7 @@
     containerlab
 
     zerotierone
+    lutris
 
     wl-clipboard
 
