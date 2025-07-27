d04925196fcd846cac9fa69f2f4af39decd0c8ae
agegon <feedcreep@yandex.ru>
2025-07-27 18:05:22 +0300

    add some programs for gaming

--- nixos-modules/gaming.nix
+++ nixos-modules/gaming.nix
@@ -7,12 +7,17 @@
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
     localNetworkGameTransfers.openFirewall = true;
+    gamescopeSession.enable = true;
   };
+
+  # Gamemode
+  programs.gamemode.enable = true;
 
   # System Packages for Gaming
   environment.systemPackages = with pkgs; [
     # steam-run # Already included via programs.steam
     lutris
     bottles
+    mangohud
   ];
 }
