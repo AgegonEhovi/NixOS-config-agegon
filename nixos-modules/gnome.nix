6dbd642f86c73c0dc03ae2b7dc088f4ee52bad2a
agegon <feedcreep@yandex.ru>
2025-09-10 09:38:35 +0300

    add cursor theme for niri

--- nixos-modules/gnome.nix
+++ nixos-modules/gnome.nix
@@ -13,7 +13,7 @@
   security.polkit.enable = true; # polkit
   services.gnome.gnome-keyring.enable = true; # secret service
   security.pam.services.swaylock = {};
-  environment.systemPackages = with pkgs; [ alacritty fuzzel swaylock mako swayidle swaybg xwayland-satellite dunst];
+  environment.systemPackages = with pkgs; [ alacritty fuzzel swaylock mako swayidle swaybg xwayland-satellite dunst vanilla-dmz];
 
   # Configure keymap in X11
   services.xserver.xkb = {
