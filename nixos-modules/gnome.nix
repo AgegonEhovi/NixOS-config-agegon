5e3fab58ec63e42187f1e2ea617e70b74af7b2d4
agegon <feedcreep@yandex.ru>
2025-10-03 22:45:16 +0300

    123

--- nixos-modules/gnome.nix
+++ nixos-modules/gnome.nix
@@ -13,8 +13,10 @@
   security.polkit.enable = true; # polkit
   services.gnome.gnome-keyring.enable = true; # secret service
   security.pam.services.swaylock = {};
-  environment.systemPackages = with pkgs; [ alacritty fuzzel swaylock mako swayidle swaybg xwayland-satellite dunst vanilla-dmz];
-
+  environment.systemPackages = with pkgs; [ alacritty fuzzel swaylock mako swayidle swaybg xwayland-satellite dunst vanilla-dmz xdg-desktop-portal xdg-desktop-portal-wlr];
+  programs.xwayland.enable = true;
+  
+  
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us,ru";
